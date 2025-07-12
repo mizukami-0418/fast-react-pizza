5736b215738a1b413646840b7712bd1ab057c862
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-07-13 07:55:08 +0900

    Errorコンポーネントを修正し、エラーメッセージを表示する機能を追加しました。また、App.jsxでエラーハンドリングを設定しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home.jsx";
+import Error from "./ui/Error.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
@@ -10,6 +11,8 @@ import AppLayout from "./ui/AppLayout.jsx";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
+
     children: [
       {
         path: "/",
